c4462527809226b6514891f73bd67a8d68afd657
flazer99 <50317684+flazer99@users.noreply.github.com>
2020-05-18 01:34:56 +0530

    fetch()

--- script.py
+++ script.py
@@ -5,17 +5,7 @@ from flask import *
 
 app = Flask(__name__)
 
-def restructure(buff):
-    data = [x.split(',') for x in buff.read().split('\n') if(len(x.split(',')) >= 2)]
-    return data
-
-@app.route("/")
-def display():
-    global details
-    return render_template('covid-main.html', data = details)
-
-
-if(__name__ == '__main__'):
+def fetch():
     link = "https://api.covid19india.org/data.json"
 
     r = requests.get(url = link)
@@ -28,5 +18,13 @@ if(__name__ == '__main__'):
     for i in y:
         res = [i['state'], i['active'], i['deaths'], i['recovered'], i['confirmed'], i['lastupdatedtime']]
         details.append(res)
+    return details
 
+@app.route("/")
+def display():
+    details = fetch()
+    return render_template('covid-main.html', data = details)
+
+
+if(__name__ == '__main__'):
     app.run(port = 8051, debug = True)
